06f37803c32c4e5d23ef5e59ab70474db1f5a404
Matheus Meissner <matheus.iembo@gmail.com>
2024-06-26 14:12:11 -0300

    update_hero_json

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const dataDoEvento = new Date("Aug 16, 2024 00:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function () {
